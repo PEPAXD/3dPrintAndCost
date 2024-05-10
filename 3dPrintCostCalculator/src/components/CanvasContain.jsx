4e03b98d52c689f6d908b0ed38a80f78db480a0b
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-05-09 22:24:39 -0300

    BasicUploadBox

--- 3dPrintCostCalculator/src/components/CanvasContain.jsx
+++ 3dPrintCostCalculator/src/components/CanvasContain.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Styles/canvasContain.css";
 
 function CanvasContain() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-white shadow-md px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <h1 className="mb-2 text-black">View3D.stl</h1>
+      <p className="mb-2 text-gray-500">Upload your 3D file here</p>
+      <br />
+      <input
+        type="file"
+        id="file"
+        name="file"
+        className="border py-2 px-3 text-gray-700 leading-tight"
+      />
     </div>
   );
 }
